a07f8f835259fe0916ef8781c05419fa3a306e1b
designdefined.by@gmail.com <designdefined.by@gmail.com>
2023-03-15 16:06:49 +0900

    fix: empty more

--- application/pages/_app.tsx
+++ application/pages/_app.tsx
@@ -1,7 +1,5 @@
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import AudioPlayer from "@/components/common/AudioPlayer";
-import MenuBar from "@/components/common/MenuBar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
